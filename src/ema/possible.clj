d3163051b99eb242b7c2bc5fee9f240a0fd9733f
siscia <simone@mweb.biz>
2014-09-01 00:20:38 +0200

    add another resource

--- src/ema/possible.clj
+++ src/ema/possible.clj
@@ -6,7 +6,9 @@
    :collections [{:name "user"
                   :authentication :second
                   :item-entries [:get :put :patch :delete]
-                  :collection-entries [:post :get]}]
+                  :collection-entries [:post :get]}
+                 {:name "sessione"
+                  :item-entries [:get :post]}]
    :authentication {:firts {:key :rest-call
                             :uri "https://www.auth.com/nvfir"}
                     :second {:key :mongo-auth
