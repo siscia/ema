573b5fb99357383532418d389cdf8d3aee964955
siscia <simone@mweb.biz>
2014-09-01 21:31:52 +0200

    minor

--- src/ema/possible.clj
+++ src/ema/possible.clj
@@ -2,13 +2,14 @@
 
 (def possible
   {:key :mongo
-   :uri "mongodb://localhost:27017/a"
+   :handler :bidi
+   :uri "mongodb://localhost:27017"
    :collections [{:name "user"
                   :authentication :second
                   :item-entries [:get :put :patch :delete]
                   :collection-entries [:post :get]}
                  {:name "sessione"
-                  :item-entries [:get :post]}]
+                  :collection-entries [:get :post]}]
    :authentication {:firts {:key :rest-call
                             :uri "https://www.auth.com/nvfir"}
                     :second {:key :mongo-auth
