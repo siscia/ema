265c46082fb7e986e079ec094080ffdbb2a6930c
siscia <simone@mweb.biz>
2014-09-01 21:32:51 +0200

    minor

--- src/ema/core.clj
+++ src/ema/core.clj
@@ -3,14 +3,14 @@
   (:require [compojure.core :refer [defroutes ANY]]
             [liberator.core :refer [resource]]
             [liberator.dev :refer [wrap-trace]]
-            [ema.mongo :as m]
+            [ema.implementation :refer [generate-handler generate-asts]]
+            [ema.mongo]
             [ema.possible :refer [possible]]
-            [ema.routes-gen :refer :all]
-            [ring.middleware.multipart-params :refer [wrap-multipart-params]]
-   ))
+            [ema.routes-gen]
+            [ring.middleware.multipart-params :refer [wrap-multipart-params]]))
 
 (def example
-  (generate-bidi-handler (m/generate-asts possible)))
+  (generate-handler (generate-asts possible)))
 
 (def app
   (-> example
