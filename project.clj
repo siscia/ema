cf02f6680bc2bcd47d3a3986b6aeb5ca7755b4f8
siscia <simone@mweb.biz>
2014-10-15 10:18:51 +0200

    update project dependencies

--- project.clj
+++ project.clj
@@ -5,15 +5,17 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [liberator "0.12.2"]
-                 [compojure "1.2.0"] ;; need to be removed
                  [http-kit "2.1.19"]
-                 [org.clojure/core.typed "0.2.68"]
+                 ;; [org.clojure/core.typed "0.2.68" :exclusions [org.clojure/core.unify]]
                  [cheshire "5.3.1"]
-                 [ring "1.3.1"]
-                 [ring/ring-defaults "0.1.2"]
-                 [ring/ring-json "0.3.1"]
+                 [ring "1.3.1" :exclusions [org.clojure/java.classpath
+                                            hiccup
+                                            joda-time]]
                  [com.novemberain/monger "2.0.0"]
-                 [bidi "1.10.5"]
+                 [bidi "1.10.5" :exclusions [com.google.guava/guava
+                                             org.clojure/data.json
+                                             joda-time
+                                             org.clojure/tools.reader]]
                  [ring-basic-authentication "1.0.5"]
                  [org.mindrot/jbcrypt "0.3m"]]
   :profiles {:dev {:dependencies [[midje "1.6.3"]]}}
